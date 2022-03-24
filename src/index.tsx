c150b6b173d00171a03c9a438aa36c3005839397
kde15 <kde15150@gmail.com>
2022-03-24 13:32:39 +0900

    fix: change max line length → 120

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-    <div>this is a template.</div>,
-    document.getElementById("root")
-);
+ReactDOM.render(<div>this is a template.</div>, document.getElementById("root"));
